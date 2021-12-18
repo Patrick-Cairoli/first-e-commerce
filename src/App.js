c91bacf722736c653333baca96a63d5f72fe46c9
unknown <patrick.cairoli@gmail.com>
2021-12-18 15:40:11 -0800

    updated menu-tems to navigate using react-router-dom@5.2.0 to enable withRouter() HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+
+import { Switch, Route } from 'react-router-dom'
+
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-        <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
